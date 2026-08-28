9b15fe6d54a5a4f4bd7c27063d29e5b1a8cd1593
Karen <karenramos2006.18@gmail.com>
2026-08-27 20:31:56 -0600

    Segundo ejercicio terminado

---
+++ api-estudiantes/src/main/java/com/example/api_estudiantes/ApiEstudiantesApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_estudiantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEstudiantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEstudiantesApplication.class, args);
+	}
+
+}
---
+++ api-estudiantes/src/test/java/com/example/api_estudiantes/ApiEstudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_estudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEstudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
